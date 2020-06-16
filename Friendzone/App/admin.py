d7ea89e7769f22ff243336b311c7e3d0eaa892ba
Kamal-prog-code <karrerajisyam123@gmail.com>
2020-06-16 15:11:25 +0530

    Delete

--- Friendzone/App/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from App.models import  *
-# Register your models here.
-admin.site.register(Account_Holder)
-admin.site.register(Account)
--- Friendzone/static/js/base.js
+++
@@ -1,4 +0,0 @@
-function fun_1(){
-    x = document.querySelector(".navbar")
-    x.style.backgroundColor="rgb(245, 244, 244)"
-}
